f0701089c0bd7022082ac09787f7e206942f0ce6
admiaro22 <aisnae1@lsu.edu>
2018-04-18 18:38:12 -0500

    paid unpaid internship

---
+++ paid unpaid internship.R
@@ -0,0 +1,24 @@
+
+library(tidyverse)
+source('conn.R')
+
+
+#paid unpaid intership
+
+
+paid<- tbl(conStudent,'internship') %>% inner_join(tbl(conStudent,'graduation')) %>% select(graduationId,jobTitle,internshipPaid) %>% 
+  filter(internshipPaid == 'Yes')
+
+
+
+
+unpaid <- tbl(conStudent,'internship') %>% inner_join(tbl(conStudent,'graduation')) %>% select(graduationId,jobTitle,internshipPaid) %>% 
+  filter(internshipPaid == 'No')
+
+total<- tbl(conStudent,'internship') %>% inner_join(tbl(conStudent,'graduation')) %>% select(graduationId,jobTitle,internshipPaid)%>% count(internshipPaid) %>% 
+  filter(!is.na(internshipPaid))
+
+paidcol<- tbl(conStudent,'internship') %>% inner_join(tbl(conStudent,'graduation')) %>% select(graduationId,jobTitle,internshipPaid)%>% count(internshipPaid) %>% 
+  filter(!is.na(internshipPaid)) %>% collect() %>% 
+  ggplot()+
+  geom_col(aes(internshipPaid, n))
