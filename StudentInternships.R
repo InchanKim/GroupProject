6b2e51fd37fdb2e44ad1e405246834821b2eda37
Inchan Kim <ikim7@lsu.edu>
2018-04-18 20:14:26 -0500

    324

--- StudentInternships.R
+++ StudentInternships.R
@@ -6,8 +6,25 @@ graduation <- tbl(conStudent, 'graduation')
 
 right_join(internship, graduation, by = c('graduationId', 'graduationId')) %>%
   select(graduationId, internshipId) %>% mutate(interns = !is.na(internshipId)) %>%
-  count(graduationId, interns) %>% mutate(interns = interns*n) %>% select(graduationId, interns) -> x
+  count(graduationId, interns) %>% mutate(interns = interns*n) %>% select(graduationId, interns) %>% collect() -> xxx
 
 right_join(internship, graduation, by = c('graduationId', 'graduationId')) %>%
-  select(graduationId, internshipId) %>% mutate(interns = !is.na(internshipId)) %>% count(interns) -> y
+  select(graduationId, internshipId) %>% mutate(interns = !is.na(internshipId)) %>% count(interns) %>% collect() -> yyy
+
+right_join(internship, graduation, by = c('graduationId', 'graduationId')) %>%
+  select(graduationId, internshipId) %>% mutate(interns = !is.na(internshipId)) %>% select(graduationId, interns) %>%
+  count(interns)
+  
+yyy %>% 
+  ggplot() +
+  geom_col(aes(x = interns, y = n)) -> yGraph
+
+xxx %>% 
+  ggplot() +
+  geom_bar(aes(interns)) -> xGraph
+
+source("salaryBrackets.R")
+fullTime
+
+xxx %>% collect() %>%  inner_join(yy, by = "graduationId")
 
