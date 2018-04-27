f349f3429aa1dbfeecf0ef5552e1de36cbc33a42
Inchan Kim <ikim7@lsu.edu>
2018-04-26 19:46:12 -0500

    11111

--- StudentInternships.R
+++ StudentInternships.R
@@ -20,6 +20,10 @@ right_join(internship, graduation, by = 'graduationId') %>%
   select(graduationId, interns) %>%
   count(interns)
 
+right_join(internship, graduation, by = 'graduationId') %>%
+  select(graduationId, internshipId) %>% mutate(interns = !is.na(internshipId)) %>%
+  select(graduationId, interns) %>% filter(interns != T) -> noInterns
+
 yyy %>% 
   ggplot() +
   geom_col(aes(x = interns, y = n)) -> yGraph
@@ -32,9 +36,9 @@ source("salaryBrackets.R")
 
 xxx %>% filter(interns == 0) %>% nrow() #There are 15174 students who have not done internship.
 xxx %>% filter(interns == 1) %>% nrow() #There are 2529 studnets who have done an internship.
-xxx %>% filter(interns == 2) %>% nrow() #There are 1165 students who have done internship 2 times.
-xxx %>% filter(interns == 3) %>% nrow() #There are 1254 students who have done internship 3 times.
-xxx %>% filter(interns == 4) %>% nrow() #There are 33 students who have done internship 4 times.
+xxx %>% filter(interns == 2) %>% nrow() #There are 1165 students who have done 2 internships.
+xxx %>% filter(interns == 3) %>% nrow() #There are 1254 students who have done 3 internships.
+xxx %>% filter(interns == 4) %>% nrow() #There are 33 students who have done 4 internships.
 
 
 xxx %>% collect() %>%  right_join(yy, by = "graduationId") %>% 
@@ -43,8 +47,4 @@ xxx %>% collect() %>%  right_join(yy, by = "graduationId") %>%
   ggtitle(label = "Internships")
 
 
-xxx %>% collect() %>%  inner_join(yy, by = "graduationId") -> xxxx
-
-
-xxxx %>% inner_join(majors, by = "graduationId")
-
+xxx %>% collect() %>%  inner_join(yy, by = "graduationId") %>% inner_join(majors, by = "graduationId")
